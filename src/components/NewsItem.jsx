b10f13e04f492bb20ed2fecdad90b0d90d20afb5
utkarsh gupta <utkarshg9981@gmail.com>
2024-04-12 09:58:42 +0530

    width change

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -6,12 +6,12 @@ function NewsItem({title,description,src,url}) {
   return (
     
 
-    <Card className='bg-dark text-light mb-3 d-inline-block mx-3 my-3 px-2 py-2 ' style={{ width : '345px'   }}>
-      <Card.Img variant="top" style={{height: '200px', width: '325px'}} src={src?src:News } />
+    <Card className='bg-dark text-light mb-3 d-inline-block mx-2 my-3 px-2 py-2 ' style={{ width : '299px'   }}>
+      <Card.Img variant="top" style={{height: '200px', width: '284px'}} src={src?src:News } />
       <Card.Body>
         <Card.Title>{title.slice(0,50)}</Card.Title>
         <Card.Text>
-         {description ? description.slice(0,90) : "this is a description of this news"}
+         {description ? description.slice(0,90) : "this is a description of this news . The news will appear soon .You can refresh the page"}
         </Card.Text>
         <a href={url} target="_blank" rel="noopener noreferrer">
 
